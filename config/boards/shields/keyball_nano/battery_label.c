17d0e8ca5d09d9419419ec3c49b0fdd3cfdc3ab7
Kyle <ababypenguin@gmail.com>
2026-09-17 23:14:47 -0700

    keyball44: battery_label: include zmk/display.h for widget listener macro

--- config/boards/shields/keyball_nano/battery_label.c
+++ config/boards/shields/keyball_nano/battery_label.c
@@ -14,6 +14,7 @@
 LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 
 #include <zmk/battery.h>
+#include <zmk/display.h>
 #include <zmk/event_manager.h>
 #include <zmk/events/battery_state_changed.h>
 #include <zmk/usb.h>
